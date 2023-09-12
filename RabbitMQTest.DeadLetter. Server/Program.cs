2b3f75d206f9beb1c3c934ecdb31d16a99b09c92
kjun <keisoft.kjun@gmail.com>
2023-09-13 00:43:10 +0900

    RabbitMQTest.DeadLetter

---
+++ RabbitMQTest.DeadLetter. Server/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RabbitMQTest.DeadLetter._Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ RabbitMQTest.DeadLetter.Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+using RabbitMQ.Client;
+
+namespace RabbitMQTest.DeadLetter.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using var connection = factory.CreateConnection();
+
+            using var channel = connection.CreateModel();
+
+            while (true)
+            {
+                channel.ExchangeDeclare(
+                    exchange: "mainexchange",
+                    type: ExchangeType.Direct);
+
+                var message = Console.ReadLine();
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("mainexchange", "test", null, body);
+
+                Console.WriteLine($"Send message: {message}");
+            }
+        }
+    }
+}
